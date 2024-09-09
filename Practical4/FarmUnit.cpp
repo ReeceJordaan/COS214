dc315d6046961db6c061947a5447c536ef214c2b
salomeboop <133574843+salomeboop@users.noreply.github.com>
2024-09-09 14:07:44 +0200

    Update FarmUnit.cpp

--- Practical4/FarmUnit.cpp
+++ Practical4/FarmUnit.cpp
@@ -84,7 +84,17 @@ void FarmUnit::callTruck() {
     std::cout << "Invalid object type. Call this on a CropField/Barn object." << std::endl;
 }
 
-void FarmUnit::applyFertilizer(){
-    std::cout<<"Applying fertilizer...\n";
-    std::cout<<"Fertilizer applied.\n";
+void FarmUnit::applyFertilizer() {
+    std::cout << "Applying fertilizer...\n";
+    std::cout << "Fertilizer applied.\n";
+
+    currentCapacity += currentCapacity * 0.1; //Fertilizer increased the number of crops produced
+
+    //Ensures that current capacity does not exceed the total capacity
+    if (currentCapacity > totalCapacity) {
+        currentCapacity = totalCapacity;
+    }
+
+    std::cout << "Current capacity after applying fertilizer: " 
+              << currentCapacity << " (out of " << totalCapacity << " total capacity)\n";
 }
