8fe96073699a89dfcabd1edafd57f97db68d1130
salomeboop <133574843+salomeboop@users.noreply.github.com>
2024-09-09 12:51:52 +0200

    Update BarnDecorator.cpp

--- Practical4/BarnDecorator.cpp
+++ Practical4/BarnDecorator.cpp
@@ -6,7 +6,21 @@ BarnDecorator::BarnDecorator(FarmUnit* unit) : Decorator(unit) {
 
 }
 
-int BarnDecorator::getLeftoverCapacity() {
+void BarnDecorator::harvest(){
+    if (farmUnit->getCurrentCapacity() > 0) {
+        cout<<"Harvesting "<<farmUnit->getCurrentCapacity()<<" units of "; //<<cropType<<endl;
+        farmUnit->setCurrentCapacity(0); //Reset after harvest
+    } 
+    else {
+        cout<<"No crops to harvest!"<<endl;
+    }
+}
+
+void BarnDecorator::increaseProduction(){
+    
+}
+
+int BarnDecorator::getLeftoverCapacity(){
     return farmUnit->getTotalCapacity() - farmUnit->getCurrentCapacity();
 }
 
