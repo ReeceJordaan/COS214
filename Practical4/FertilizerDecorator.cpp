728471333a136e020730c24ada36c56682099e7b
salomeboop <133574843+salomeboop@users.noreply.github.com>
2024-09-09 14:21:29 +0200

    Update FertilizerDecorator.cpp

--- Practical4/FertilizerDecorator.cpp
+++ Practical4/FertilizerDecorator.cpp
@@ -5,19 +5,6 @@ using namespace std;
 FertilizerDecorator::FertilizerDecorator(FarmUnit* unit) : Decorator(unit) {}
 
 void FertilizerDecorator::harvest(){
-    
-}
-
-void FertilizerDecorator::increaseProduction(){
-    farmUnit->applyFertilizer();
-    
-}
-
-int FertilizerDecorator::getLeftoverCapacity(){
-    return farmUnit->getTotalCapacity() - farmUnit->getCurrentCapacity();
-}
-
-/*void BarnDecorator::harvest(){
     if (farmUnit->getCurrentCapacity() > 0) {
         cout<<"Harvesting "<<farmUnit->getCurrentCapacity()<<" units of crops."<<endl;
         farmUnit->setCurrentCapacity(0); //Reset after harvest
@@ -27,15 +14,14 @@ int FertilizerDecorator::getLeftoverCapacity(){
     }
 }
 
-void BarnDecorator::increaseProduction(){
-    farmUnit->setTotalCapacity(farmUnit->getTotalCapacity() * 0.4); //increased the total capacity by 40%
-    cout<<"Barn boosts production capacity by increasing total capacity by 40%. Thus more crops to harvest and store in the barn."<<endl;
-    cout<<"Increased production: New total capacity is "<<farmUnit->getTotalCapacity()<<endl;
+void FertilizerDecorator::increaseProduction(){
+    farmUnit->applyFertilizer();
+    cout<<"Thus crop yield has increased by 10%."<<endl;
 }
 
-int BarnDecorator::getLeftoverCapacity(){
+int FertilizerDecorator::getLeftoverCapacity(){
     return farmUnit->getTotalCapacity() - farmUnit->getCurrentCapacity();
-}*/
+}
 
 void FertilizerDecorator::print() {
     std::cout << "Print in FertilizerDecorator." << std::endl;
