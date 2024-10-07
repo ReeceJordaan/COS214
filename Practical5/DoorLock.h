cf4462f92d5238839ddbd8da397335ba5b3e8fb0
salomeboop <133574843+salomeboop@users.noreply.github.com>
2024-10-07 21:08:36 +0200

    Update DoorLock.h

--- Practical5/DoorLock.h
+++ Practical5/DoorLock.h
@@ -6,25 +6,87 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * @class DoorLock
+ * @brief A smart door lock device that can be locked or unlocked and responds to changes in the DoorSensor's state.
+ *
+ * The DoorLock class implements various design patterns, including Composite, Adapter, Command, and Observer.
+ * It performs actions based on the state of the door sensor it is observing.
+ *
+ * @implements This class inherits from SmartDevice and observes a DoorSensor.
+ *
+ * @ingroup Pattern Wizards
+ * 
+ * @attention Ensure the DoorSensor is properly initialized before using the DoorLock.
+ * 
+ * @warning Improper initialization of the DoorSensor could lead to undefined behavior.
+ * 
+ * @note The device type for this class is always "Door Lock".
+ * 
+ * @version 1.0
+ * 
+ * @date 07 October 2024
+ * 
+ * @author Salome Kalaka
+ * 
+ * @see SmartDevice class, DoorSensor class, performAction(), getDeviceType(), update()
+ */
 class DoorLock : public SmartDevice
 {
     public:
+        /**
+         * @brief Constructor for the DoorLock class.
+         * 
+         * Initializes the DoorLock with the given lock status.
+         * 
+         * @param status A boolean representing whether the door is locked (true) or unlocked (false).
+         */
         DoorLock(bool status);
+
+        /**
+         * @brief Destructor for the DoorLock class.
+         * 
+         * Cleans up any resources used by the DoorLock.
+         */
         virtual ~DoorLock();
 
-        //! Locks/Unlocks the door.
+        /**
+         * @brief Locks or unlocks the door.
+         * 
+         * This method toggles the lock status of the door. It is part of the Command pattern.
+         */
         virtual void performAction();
-        //! Returns "Door Lock" as the device type.
+
+        /**
+         * @brief Returns the device type.
+         * 
+         * This method always returns "Door Lock" as the type of the device.
+         * 
+         * @return A string representing the device type.
+         */
         virtual string getDeviceType();
-        //! Updates doorSensorState and performs an action if necessary.
+
+        /**
+         * @brief Updates the state based on the DoorSensor's status.
+         * 
+         * This method checks the DoorSensor's state and performs actions if necessary. It is part of the Observer pattern.
+         */
         virtual void update();
 
     protected:
-        //! State of the DoorSensor that DoorLock is observing.
+        /**
+         * @brief The state of the DoorSensor being observed.
+         * 
+         * Represents whether the door is open or closed, as reported by the DoorSensor.
+         */
         bool doorSensorState;
-        //! Pointer to the DoorSensor that DoorLock is observing.
+
+        /**
+         * @brief Pointer to the DoorSensor being observed.
+         * 
+         * This pointer allows the DoorLock to observe and react to changes in the DoorSensor's state.
+         */
         DoorSensor* doorSensor;
 };
-
 
 #endif
